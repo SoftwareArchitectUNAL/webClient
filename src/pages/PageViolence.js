e2ce3fa87844940513725e583f85268d92d2cfda
rafasalgado <rasalgadol@unal.edu.co>
2018-10-23 17:41:29 +0000

    push desde cloud cambio de url

--- src/pages/PageViolence.js
+++ src/pages/PageViolence.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from "react-apollo";
 
 
 const client = new ApolloClient({
-  uri: 'http://35.231.198.209:5000/graphql'
+  uri: 'http://35.237.81.177:5000/graphql'
 });
 
 
